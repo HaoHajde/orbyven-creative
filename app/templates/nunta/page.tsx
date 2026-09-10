6f79d85578eb1cf38f0c17f522715f63d940879c
HaoHajde <hajde.soap@gmail.com>
2026-09-10 03:10:25 +0300

    Point wedding template route to generic demo

--- app/templates/nunta/page.tsx
+++ app/templates/nunta/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
-export default function LegacyWeddingTemplateRoute() {
-  redirect("/demo/nunta/diana-florin");
+export default function WeddingTemplateRoute() {
+  redirect("/demo/nunta/elegant");
 }
